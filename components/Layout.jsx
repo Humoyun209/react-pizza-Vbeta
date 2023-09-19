31c57761b4dfb051698c7e9c262a0684d2acde33
Humoyun <h.ahmedov209@gmail.com>
2023-09-19 14:47:16 +0500

    ⚙️  Improved code readability

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <main className='py-[53px] pl-[20px] pr-[15px] sm:pl-[50px] sm:[pr-25px] md:pl-[77px] md:pr-[38px] my-[43px] max-w-[1340px] mx-auto bg-white'>
-        <Outlet />
+    <main className="py-[53px] pl-[20px] pr-[15px] sm:pl-[50px] sm:[pr-25px] md:pl-[77px] md:pr-[38px] my-[43px] max-w-[1340px] mx-auto bg-white">
+      <Outlet />
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/BasketPage.jsx
+++ pages/BasketPage.jsx
@@ -24,7 +24,7 @@ const BasketPage = () => {
 
   return (
     <Fragment>
-       <Navbar />
+      <Navbar />
       {dataCart.length ? (
         <Fragment>
           <HeaderBasket />
